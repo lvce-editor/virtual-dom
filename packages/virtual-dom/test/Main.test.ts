26c074e4f84a8045df80849881c8e704208b3ab5
Le Vivilet <levivlet@pm.me>
2024-07-07 19:24:49 +0200

    feature: add virtualDom function

--- packages/virtual-dom/test/Main.test.ts
+++ packages/virtual-dom/test/Main.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@jest/globals'
 import * as Main from '../src/parts/Main/Main.ts'
 
-test('VirtualDomElement', () => {
-  expect(typeof Main.VirtualDomElement).toBeDefined()
+test('render', () => {
+  expect(typeof Main.render).toBeDefined()
 })
