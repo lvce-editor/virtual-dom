1b7d1ec1926aca60212cd188e1947c05e96c7a09
Le Vivilet <levivlet@pm.me>
2024-07-15 19:37:16 +0200

    feature: add rememberFocus api function

---
+++ packages/virtual-dom/src/parts/RememberFocus/RememberFocus.ts
@@ -0,0 +1,57 @@
+import * as ComponentUid from '../ComponentUid/ComponentUid.ts'
+import * as VirtualDom from '../VirtualDom/VirtualDom.ts'
+
+const queryInputs = ($Viewlet: HTMLElement) => {
+  return [...$Viewlet.querySelectorAll('input, textarea')]
+}
+
+export const rememberFocus = (
+  $Viewlet: HTMLElement,
+  dom: any[],
+  eventMap: any,
+  uid = 0,
+) => {
+  // TODO replace this workaround with
+  // virtual dom diffing
+  const oldLeft = $Viewlet.style.left
+  const oldTop = $Viewlet.style.top
+  const oldWidth = $Viewlet.style.width
+  const oldHeight = $Viewlet.style.height
+  // @ts-expect-error
+  const focused = document.activeElement.getAttribute('name')
+  const $$Inputs = queryInputs($Viewlet)
+  const inputMap = Object.create(null)
+  for (const $Input of $$Inputs) {
+    // @ts-ignore
+    inputMap[$Input.name] = $Input.value
+  }
+  if (uid) {
+    const $New = VirtualDom.render(dom, eventMap).firstChild
+    ComponentUid.setComponentUid($New, uid)
+    // @ts-ignore
+    $Viewlet.replaceWith($New)
+    // @ts-ignore
+    $Viewlet = $New
+  } else {
+    VirtualDom.renderInto($Viewlet, dom, eventMap)
+  }
+  const $$NewInputs = queryInputs($Viewlet)
+  for (const $Input of $$NewInputs) {
+    // @ts-ignore
+    $Input.value = inputMap[$Input.name] || $Input.value || ''
+  }
+  if (focused) {
+    const $Focused = $Viewlet.querySelector(`[name="${focused}"]`)
+    if ($Focused) {
+      // @ts-ignore
+      $Focused.focus()
+    }
+  }
+
+  $Viewlet.style.top = oldTop
+  $Viewlet.style.left = oldLeft
+  $Viewlet.style.height = oldHeight
+  $Viewlet.style.width = oldWidth
+
+  return $Viewlet
+}
