753bd137347f1084a4ab52e66f28109079c961e4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-15 22:22:42 +0200

    feature: sort imports in build file (#30)

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -1,7 +1,7 @@
+import { execa } from 'execa'
 import { cp, mkdir, readFile, rm, writeFile } from 'node:fs/promises'
 import path, { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { execa, execaNode } from 'execa'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const root = path.join(__dirname, '..', '..', '..')
