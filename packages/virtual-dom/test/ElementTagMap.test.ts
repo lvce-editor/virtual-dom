d4ebab381e6a7db071dc690557c19bb8f0bc4f32
Le Vivilet <levivlet@pm.me>
2024-11-24 16:52:13 +0100

    feature: add more elementTagMap tests

--- packages/virtual-dom/test/ElementTagMap.test.ts
+++ packages/virtual-dom/test/ElementTagMap.test.ts
@@ -98,3 +98,99 @@ test('ElementTagMap - throws error for unknown element type', () => {
     'element tag not found 999',
   )
 })
+
+test('ElementTagMap - maps list elements', () => {
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Ul)).toBe(
+    ElementTags.Ul,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Ol)).toBe(
+    ElementTags.Ol,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Li)).toBe(
+    ElementTags.Li,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Dl)).toBe(
+    ElementTags.Dl,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Dd)).toBe(
+    ElementTags.Dd,
+  )
+})
+
+test('ElementTagMap - maps text formatting elements', () => {
+  expect(ElementTagMap.getElementTag(VirtualDomElements.P)).toBe(ElementTags.P)
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Pre)).toBe(
+    ElementTags.Pre,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Span)).toBe(
+    ElementTags.Span,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.I)).toBe(ElementTags.I)
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Kbd)).toBe(
+    ElementTags.Kbd,
+  )
+})
+
+test('ElementTagMap - maps media elements', () => {
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Img)).toBe(
+    ElementTags.Img,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Video)).toBe(
+    ElementTags.Video,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Figure)).toBe(
+    ElementTags.Figure,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Figcaption)).toBe(
+    ElementTags.Figcaption,
+  )
+})
+
+test('ElementTagMap - maps inline elements', () => {
+  expect(ElementTagMap.getElementTag(VirtualDomElements.A)).toBe(ElementTags.A)
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Br)).toBe(
+    ElementTags.Br,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Cite)).toBe(
+    ElementTags.Cite,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Data)).toBe(
+    ElementTags.Data,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Time)).toBe(
+    ElementTags.Time,
+  )
+})
+
+test('ElementTagMap - maps table structure elements', () => {
+  expect(ElementTagMap.getElementTag(VirtualDomElements.ColGroup)).toBe(
+    ElementTags.ColGroup,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Col)).toBe(
+    ElementTags.Col,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Tfoot)).toBe(
+    ElementTags.Tfoot,
+  )
+})
+
+test('ElementTagMap - maps text modification elements', () => {
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Ins)).toBe(
+    ElementTags.Ins,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Del)).toBe(
+    ElementTags.Del,
+  )
+})
+
+test('ElementTagMap - maps structural elements', () => {
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Hr)).toBe(
+    ElementTags.Hr,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Aside)).toBe(
+    ElementTags.Aside,
+  )
+  expect(ElementTagMap.getElementTag(VirtualDomElements.Search)).toBe(
+    ElementTags.Search,
+  )
+})
