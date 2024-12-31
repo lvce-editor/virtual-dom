ef24835c5cf57a2e145b9be4496b5d7854df29f3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-31 14:36:46 +0100

    feature: use explicit return types (#66)

--- packages/virtual-dom/src/parts/VirtualDomElement/VirtualDomElement.ts
+++ packages/virtual-dom/src/parts/VirtualDomElement/VirtualDomElement.ts
@@ -2,18 +2,26 @@ import * as ElementTagMap from '../ElementTagMap/ElementTagMap.ts'
 import * as VirtualDomElementProps from '../VirtualDomElementProps/VirtualDomElementProps.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
-const renderDomTextNode = (element) => {
+const renderDomTextNode = (element: any): Text => {
   return document.createTextNode(element.text)
 }
 
-const renderDomElement = (element, eventMap, newEventMap) => {
+const renderDomElement = (
+  element: any,
+  eventMap: any,
+  newEventMap: any,
+): HTMLElement => {
   const tag = ElementTagMap.getElementTag(element.type)
   const $Element = document.createElement(tag)
   VirtualDomElementProps.setProps($Element, element, eventMap, newEventMap)
   return $Element
 }
 
-export const render = (element, eventMap, newEventMap?: any) => {
+export const render = (
+  element: any,
+  eventMap: any,
+  newEventMap?: any,
+): Node => {
   switch (element.type) {
     case VirtualDomElements.Text:
       return renderDomTextNode(element)
