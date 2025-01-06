8f2ec7e64bdd0a76f4cc08f631a96082b260d6fd
Le Vivilet <levivlet@pm.me>
2025-01-06 18:24:33 +0100

    feature: add patch test

---
+++ packages/virtual-dom-worker/test/Patch.test.ts
@@ -0,0 +1,14 @@
+import { expect, test } from '@jest/globals'
+import type { Patch } from '../src/parts/Patch/Patch.ts'
+import * as PatchType from '../src/parts/PatchType/PatchType.ts'
+
+test('patch type safety', () => {
+  const patches: Patch[] = [
+    {
+      type: PatchType.SetText,
+      index: 0,
+      value: 'test',
+    },
+  ]
+  expect(patches).toBeDefined()
+})
