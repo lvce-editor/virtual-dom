39f472ad5f4e0c2d111c6877c7c41906c9be4670
Le Vivilet <levivlet@pm.me>
2025-01-06 18:26:38 +0100

    feature: add removeAttribute patch

--- packages/virtual-dom-worker/src/parts/Patch/Patch.ts
+++ packages/virtual-dom-worker/src/parts/Patch/Patch.ts
@@ -1,5 +1,10 @@
 import type { AttributePatch } from '../AttributePatch/AttributePatch.ts'
 import type { ReplacePatch } from '../ReplacePatch/ReplacePatch.ts'
 import type { TextPatch } from '../TextPatch/TextPatch.ts'
+import type { RemoveAttributePatch } from '../RemoveAttributePatch/RemoveAttributePatch.ts'
 
-export type Patch = TextPatch | AttributePatch | ReplacePatch
+export type Patch =
+  | TextPatch
+  | AttributePatch
+  | ReplacePatch
+  | RemoveAttributePatch
--- packages/virtual-dom-worker/src/parts/PatchType/PatchType.ts
+++ packages/virtual-dom-worker/src/parts/PatchType/PatchType.ts
@@ -1,3 +1,4 @@
 export const SetText = 1
 export const Replace = 2
 export const SetAttribute = 3
+export const RemoveAttribute = 4
