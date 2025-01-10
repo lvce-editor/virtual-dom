8a290668aa01ab4fe3a9184ea9b35e6d26e3f99c
Le Vivilet <levivlet@pm.me>
2025-01-10 23:05:32 +0100

    feature: add remove patch type

--- packages/virtual-dom-worker/src/parts/Patch/Patch.ts
+++ packages/virtual-dom-worker/src/parts/Patch/Patch.ts
@@ -2,9 +2,11 @@ import type { AttributePatch } from '../AttributePatch/AttributePatch.ts'
 import type { ReplacePatch } from '../ReplacePatch/ReplacePatch.ts'
 import type { TextPatch } from '../TextPatch/TextPatch.ts'
 import type { RemoveAttributePatch } from '../RemoveAttributePatch/RemoveAttributePatch.ts'
+import type { RemovePatch } from '../RemovePatch/RemovePatch.ts'
 
 export type Patch =
   | TextPatch
   | AttributePatch
   | ReplacePatch
   | RemoveAttributePatch
+  | RemovePatch
--- packages/virtual-dom-worker/src/parts/PatchType/PatchType.ts
+++ packages/virtual-dom-worker/src/parts/PatchType/PatchType.ts
@@ -2,3 +2,4 @@ export const SetText = 1
 export const Replace = 2
 export const SetAttribute = 3
 export const RemoveAttribute = 4
+export const Remove = 5
