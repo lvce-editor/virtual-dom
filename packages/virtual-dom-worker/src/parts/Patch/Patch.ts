2ab4648760cf191f367d88e3d544edebe62f071a
Le Vivilet <levivlet@pm.me>
2025-01-10 23:09:07 +0100

    feature: add addPatch

--- packages/virtual-dom-worker/src/parts/Patch/Patch.ts
+++ packages/virtual-dom-worker/src/parts/Patch/Patch.ts
@@ -1,8 +1,9 @@
+import type { AddPatch } from '../AddPatch/AddPatch.ts'
 import type { AttributePatch } from '../AttributePatch/AttributePatch.ts'
-import type { ReplacePatch } from '../ReplacePatch/ReplacePatch.ts'
-import type { TextPatch } from '../TextPatch/TextPatch.ts'
 import type { RemoveAttributePatch } from '../RemoveAttributePatch/RemoveAttributePatch.ts'
 import type { RemovePatch } from '../RemovePatch/RemovePatch.ts'
+import type { ReplacePatch } from '../ReplacePatch/ReplacePatch.ts'
+import type { TextPatch } from '../TextPatch/TextPatch.ts'
 
 export type Patch =
   | TextPatch
@@ -10,3 +11,4 @@ export type Patch =
   | ReplacePatch
   | RemoveAttributePatch
   | RemovePatch
+  | AddPatch
