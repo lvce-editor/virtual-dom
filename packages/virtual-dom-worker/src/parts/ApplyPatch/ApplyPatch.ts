c58d636f405fc3e310bb479798af8842d1014a0b
Le Vivilet <levivlet@pm.me>
2025-01-11 09:54:55 +0100

    feature: add more patch functions

--- packages/virtual-dom-worker/src/parts/ApplyPatch/ApplyPatch.ts
+++ packages/virtual-dom-worker/src/parts/ApplyPatch/ApplyPatch.ts
@@ -1,15 +1,19 @@
 import { Patch } from '../Patch/Patch.ts'
 import * as PatchType from '../PatchType/PatchType.ts'
 
-export const applyPatch = (
-  $Element: HTMLElement,
-  patches: readonly Patch[],
-) => {
+export const applyPatch = ($Element: Node, patches: readonly Patch[]) => {
   for (const patch of patches) {
     switch (patch.type) {
       case PatchType.SetAttribute:
-        $Element.setAttribute(patch.key, patch.value)
+        ;($Element as HTMLElement).setAttribute(patch.key, patch.value)
         break
+      case PatchType.RemoveAttribute:
+        ;($Element as HTMLElement).removeAttribute(patch.key)
+        break
+      case PatchType.SetText:
+        $Element.nodeValue = patch.value
+        break
+
       default:
         break
     }
--- packages/virtual-dom-worker/test/ApplyPatch.test.ts
+++ packages/virtual-dom-worker/test/ApplyPatch.test.ts
@@ -19,3 +19,30 @@ test('attribute change', () => {
   ApplyPatch.applyPatch($Node, patches)
   expect($Node.id).toBe('test')
 })
+
+test('attribute remove', () => {
+  const patches: readonly Patch[] = [
+    {
+      type: PatchType.RemoveAttribute,
+      index: 0,
+      key: 'id',
+    },
+  ]
+  const $Node = document.createElement('div')
+  $Node.id = 'test'
+  ApplyPatch.applyPatch($Node, patches)
+  expect($Node.id).toBe('')
+})
+
+test('text change', () => {
+  const patches: readonly Patch[] = [
+    {
+      type: PatchType.SetText,
+      index: 0,
+      value: 'test',
+    },
+  ]
+  const $Node = document.createTextNode('test')
+  ApplyPatch.applyPatch($Node, patches)
+  expect($Node.textContent).toBe('test')
+})
