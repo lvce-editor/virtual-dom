6c2ad128e7549f54a17a7b347ac05a925453768b
Le Vivilet <levivlet@pm.me>
2025-01-11 10:03:28 +0100

    feature: add patch helper functions

---
+++ packages/virtual-dom-worker/src/parts/PatchFunctions/PatchFunctions.ts
@@ -0,0 +1,20 @@
+export const setAttribute = (
+  $Element: HTMLElement,
+  key: string,
+  value: any,
+): void => {
+  $Element.setAttribute(key, value)
+}
+
+export const removeAttribute = ($Element: HTMLElement, key: string): void => {
+  $Element.removeAttribute(key)
+}
+
+export const setText = ($Element: Text, value: string): void => {
+  $Element.nodeValue = value
+}
+
+export const removeChild = ($Element: HTMLElement, index: number): void => {
+  const $Child = $Element.children[index]
+  $Child.remove()
+}
