af06e1fa243c0accdd15d0b2b9fe657ed95bde3f
Le Vivilet <levivlet@pm.me>
2025-01-11 18:02:47 +0100

    test

--- packages/virtual-dom-worker/test/Patch.test.ts
+++ packages/virtual-dom-worker/test/Patch.test.ts
@@ -3,7 +3,7 @@ import type { Patch } from '../src/parts/Patch/Patch.ts'
 import * as PatchType from '../src/parts/PatchType/PatchType.ts'
 
 test('patch type safety', () => {
-  const patches: Patch[] = [
+  const patches: readonly Patch[] = [
     {
       type: PatchType.SetText,
       value: 'test',
