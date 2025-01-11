5aed918f90b5179f4358dfad60afe74352a21a42
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-12 00:58:01 +0100

    feature: update types (#129)

--- packages/virtual-dom-worker/src/parts/Main/Main.ts
+++ packages/virtual-dom-worker/src/parts/Main/Main.ts
@@ -1,3 +1,4 @@
 export * from '../MergeClassNames/MergeClassNames.ts'
 export * from '../Text/Text.ts'
+export { diff } from '../VirtualDomDiff/VirtualDomDiff.ts'
 export * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
