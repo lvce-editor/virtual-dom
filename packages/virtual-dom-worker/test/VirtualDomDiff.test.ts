e57502f861352a2ad84e387214e62f978d9e2214
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-11 23:03:44 +0100

    feature: enable more diff tests (#121)

--- packages/virtual-dom-worker/test/VirtualDomDiff.test.ts
+++ packages/virtual-dom-worker/test/VirtualDomDiff.test.ts
@@ -986,7 +986,7 @@ test('diff - child removed, sibling added', () => {
   ])
 })
 
-test.skip('diff - child added, sibling removed', () => {
+test('diff - child added, sibling removed', () => {
   const oldNodes = [
     {
       type: VirtualDomElements.Div,
