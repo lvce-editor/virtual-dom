1e0223ae81c1f3c86004d37ffabff15fc271da1a
Le Vivilet <levivlet@pm.me>
2025-01-11 23:27:36 +0100

    simplify

--- packages/virtual-dom-worker/src/parts/GetKeys/GetKeys.ts
+++ packages/virtual-dom-worker/src/parts/GetKeys/GetKeys.ts
@@ -1,8 +1,10 @@
 import { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
+const isKey = (key: string): boolean => {
+  return key !== 'type' && key !== 'childCount'
+}
+
 export const getKeys = (node: VirtualDomNode): readonly string[] => {
-  const keys = Object.keys(node).filter(
-    (key) => key !== 'type' && key !== 'childCount',
-  )
+  const keys = Object.keys(node).filter(isKey)
   return keys
 }
