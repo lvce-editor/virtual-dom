55433562591ac233d17395d86bd9702ab42a9b7c
Le Vivilet <levivlet@pm.me>
2025-01-12 01:20:05 +0100

    feature: move isKey into separate file

--- packages/virtual-dom-worker/src/parts/GetKeys/GetKeys.ts
+++ packages/virtual-dom-worker/src/parts/GetKeys/GetKeys.ts
@@ -1,10 +1,7 @@
+import * as IsKey from '../IsKey/IsKey.ts'
 import { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
-const isKey = (key: string): boolean => {
-  return key !== 'type' && key !== 'childCount'
-}
-
 export const getKeys = (node: VirtualDomNode): readonly string[] => {
-  const keys = Object.keys(node).filter(isKey)
+  const keys = Object.keys(node).filter(IsKey.isKey)
   return keys
 }
