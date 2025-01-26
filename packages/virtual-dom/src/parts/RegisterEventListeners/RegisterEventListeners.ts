c0483e6abb6d7ea939e917063d95675e892026d0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-26 15:36:31 +0100

    feature: move event listener function into separate file (#144)

--- packages/virtual-dom/src/parts/RegisterEventListeners/RegisterEventListeners.ts
+++ packages/virtual-dom/src/parts/RegisterEventListeners/RegisterEventListeners.ts
@@ -1,34 +1,11 @@
-import * as ComponentUid from '../ComponentUid/ComponentUid.ts'
-import * as IpcState from '../IpcState/IpcState.ts'
-import * as NameAnonymousFunction from '../NameAnonymousFunction/NameAnonymousFunction.ts'
+import * as CreateEventListenerFunction from '../CreateEventListenerFunction/CreateEventListenerFunction.ts'
 
 const listeners = Object.create(null)
-
-const getArgs = (params, event) => {
-  return [...params]
-}
-
-const createFn = (info) => {
-  const fn = (event) => {
-    if (info.preventDefault) {
-      event.preventDefault(event)
-    }
-    const uid = ComponentUid.getComponentUidFromEvent(event)
-    const args = getArgs(info.params, event)
-    if (args.length === 0) {
-      return
-    }
-    const ipc = IpcState.getIpc()
-    ipc.send('Viewlet.executeViewletCommand', uid, ...args)
-  }
-  NameAnonymousFunction.nameAnonymousFunction(fn, info.name)
-  return fn
-}
 
 export const registerEventListeners = (id, eventListeners) => {
   const map = Object.create(null)
   for (const info of eventListeners) {
-    const fn = createFn(info)
+    const fn = CreateEventListenerFunction.createFn(info)
     map[info.name] = fn
   }
   listeners[id] = map
