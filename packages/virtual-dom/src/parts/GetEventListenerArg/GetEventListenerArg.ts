875917f4d3898f796d8cd397d58c9224f2317d60
Le Vivilet <levivlet@pm.me>
2025-01-26 16:26:07 +0100

    support more

--- packages/virtual-dom/src/parts/GetEventListenerArg/GetEventListenerArg.ts
+++ packages/virtual-dom/src/parts/GetEventListenerArg/GetEventListenerArg.ts
@@ -10,6 +10,12 @@ export const getEventListenerArg = (param: string, event: any): any => {
       return event.target.value
     case 'event.data':
       return event.data
+    case 'event.deltaMode':
+      return event.deltaMode
+    case 'event.deltaX':
+      return event.deltaX
+    case 'event.deltaY':
+      return event.deltaY
     default:
       return param
   }
