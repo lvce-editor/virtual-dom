f28ef7439bff4361ebd95d93f53fc83d1f0f03b3
Le Vivilet <levivlet@pm.me>
2025-01-29 21:52:18 +0100

    feature: support x and y event properties

--- packages/virtual-dom/src/parts/GetEventListenerArg/GetEventListenerArg.ts
+++ packages/virtual-dom/src/parts/GetEventListenerArg/GetEventListenerArg.ts
@@ -2,8 +2,12 @@ export const getEventListenerArg = (param: string, event: any): any => {
   switch (param) {
     case 'event.clientX':
       return event.clientX
+    case 'event.x':
+      return event.x
     case 'event.clientY':
       return event.clientY
+    case 'event.y':
+      return event.y
     case 'event.button':
       return event.button
     case 'event.target.value':
