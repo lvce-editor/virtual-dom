1d86d783ac734107b01b7457049ffe3955e38ac9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-22 09:23:12 +0100

    feature: use explicit return type (#166)

--- packages/virtual-dom/src/parts/GetUidTarget/GetUidTarget.ts
+++ packages/virtual-dom/src/parts/GetUidTarget/GetUidTarget.ts
@@ -1,11 +1,13 @@
 import { uidSymbol } from '../UidSymbol/UidSymbol.ts'
 
-export const getUidTarget = ($Element) => {
+export const getUidTarget = (
+  $Element: HTMLElement,
+): HTMLElement | undefined => {
   while ($Element) {
     if ($Element[uidSymbol]) {
       return $Element
     }
-    $Element = $Element.parentNode
+    $Element = $Element.parentNode as HTMLElement
   }
   return undefined
 }
--- packages/virtual-dom/src/parts/VirtualDomElementProps/VirtualDomElementProps.ts
+++ packages/virtual-dom/src/parts/VirtualDomElementProps/VirtualDomElementProps.ts
@@ -1,6 +1,6 @@
 import * as VirtualDomElementProp from '../VirtualDomElementProp/VirtualDomElementProp.ts'
 
-export const setProps = ($Element, props, eventMap, newEventMap) => {
+export const setProps = ($Element, props, eventMap, newEventMap): void => {
   for (const key in props) {
     VirtualDomElementProp.setProp(
       $Element,
