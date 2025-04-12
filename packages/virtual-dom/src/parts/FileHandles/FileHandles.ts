ce65c0aa5fae22600050e35d57491a189c1650f1
Le Vivilet <levivlet@pm.me>
2025-04-12 17:46:14 +0200

    fix: remove file handles from state when sending them

--- packages/virtual-dom/src/parts/FileHandles/FileHandles.ts
+++ packages/virtual-dom/src/parts/FileHandles/FileHandles.ts
@@ -2,14 +2,16 @@ import * as Id from '../Id/Id.ts'
 
 const state: Record<number, Promise<FileSystemHandle>> = Object.create(null)
 
-export const get = (id: number): Promise<FileSystemHandle> => {
-  return state[id]
+export const acquire = (id: number): Promise<FileSystemHandle> => {
+  const promise = state[id]
+  delete state[id]
+  return promise
 }
 
 export const getFileHandles = async (
   ids: readonly number[],
 ): Promise<readonly FileSystemHandle[]> => {
-  const promises = ids.map((id) => get(id))
+  const promises = ids.map((id) => acquire(id))
   const handles = await Promise.all(promises)
   return handles
 }
