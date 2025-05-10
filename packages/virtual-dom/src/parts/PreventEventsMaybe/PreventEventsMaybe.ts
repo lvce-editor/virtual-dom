2144d115641281d01a3b15a8378ce7aced15529a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-10 09:41:06 +0200

    feature: add new event prevent default options that prevents if the event is not from an input element (#203)

--- packages/virtual-dom/src/parts/PreventEventsMaybe/PreventEventsMaybe.ts
+++ packages/virtual-dom/src/parts/PreventEventsMaybe/PreventEventsMaybe.ts
@@ -1,5 +1,13 @@
+const isInputElement = (element: HTMLElement): boolean => {
+  return element instanceof HTMLInputElement
+}
+
 export const preventEventsMaybe = (info: any, event: any): void => {
-  if (info.preventDefault) {
+  if (info.preventDefault === 2) {
+    if (!isInputElement(event.target)) {
+      event.preventDefault()
+    }
+  } else if (info.preventDefault) {
     event.preventDefault()
   }
   if (info.stopPropagation) {
