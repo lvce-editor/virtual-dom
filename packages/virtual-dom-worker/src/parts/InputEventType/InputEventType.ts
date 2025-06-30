774d40d9343ff4d317c1b10b3165662010a2d15f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-30 12:22:14 +0200

    feature: add input event type (#230)

---
+++ packages/virtual-dom-worker/src/parts/InputEventType/InputEventType.ts
@@ -0,0 +1,15 @@
+export const InsertText = 'insertText'
+
+export const DeleteContentBackward = 'deleteContentBackward'
+
+export const DeleteContentForward = 'deleteContentForward'
+
+export const DeleteWordForward = 'deleteWordForward'
+
+export const DeleteWordBackward = 'deleteWordBackward'
+
+export const InsertLineBreak = 'insertLineBreak'
+
+export const InsertCompositionText = 'insertCompositionText'
+
+export const InsertFromPaste = 'insertFromPaste'
