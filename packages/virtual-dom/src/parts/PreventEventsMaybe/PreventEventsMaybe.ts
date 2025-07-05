8c31d7eb29c407dc7bd5fc052e4473c371cba76d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-06 00:28:48 +0200

    feature: support drag event options (#235)

--- packages/virtual-dom/src/parts/PreventEventsMaybe/PreventEventsMaybe.ts
+++ packages/virtual-dom/src/parts/PreventEventsMaybe/PreventEventsMaybe.ts
@@ -13,4 +13,14 @@ export const preventEventsMaybe = (info: any, event: any): void => {
   if (info.stopPropagation) {
     event.stopPropagation()
   }
+  if (event.dataTransfer) {
+    if (info.dragInfo) {
+      for (const item of info.dragInfo) {
+        event.dataTransfer.setData(item.type, item.data)
+      }
+    }
+    if (info.dragEffect) {
+      event.dataTransfer.effectAllowed = info.dragEffect
+    }
+  }
 }
