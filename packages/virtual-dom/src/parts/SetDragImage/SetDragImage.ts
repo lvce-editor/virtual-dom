83d66596f74dfc04252e6a5cd71a25d9b73c1a91
Le Vivilet <levivlet@pm.me>
2025-07-06 12:44:48 +0200

    type

--- packages/virtual-dom/src/parts/SetDragImage/SetDragImage.ts
+++ packages/virtual-dom/src/parts/SetDragImage/SetDragImage.ts
@@ -1,10 +1,13 @@
-export const setDragImage = (dataTransfer: DataTransfer, label: string) => {
+export const setDragImage = (
+  dataTransfer: DataTransfer,
+  label: string,
+): void => {
   const dragImage = document.createElement('div')
   dragImage.className = 'DragImage'
   dragImage.textContent = label
   document.body.append(dragImage)
   dataTransfer.setDragImage(dragImage, -10, -10)
-  const handleTimeOut = () => {
+  const handleTimeOut = (): void => {
     dragImage.remove()
   }
   setTimeout(handleTimeOut, 0)
