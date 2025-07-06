49339846e017ab209e180454e3758eb7683bcedd
Le Vivilet <levivlet@pm.me>
2025-07-06 12:44:20 +0200

    move fn

--- packages/virtual-dom/src/parts/PreventEventsMaybe/PreventEventsMaybe.ts
+++ packages/virtual-dom/src/parts/PreventEventsMaybe/PreventEventsMaybe.ts
@@ -1,7 +1,4 @@
-import { getComponentUid } from '../ComponentUid/ComponentUid.ts'
-import { getDragInfo } from '../DragInfo/DragInfo.ts'
 import { isInputElement } from '../IsInputElement/IsInputElement.ts'
-import { setDragImage } from '../SetDragImage/SetDragImage.ts'
 
 export const preventEventsMaybe = (info: any, event: any): void => {
   if (info.preventDefault === 2) {
@@ -13,19 +10,5 @@ export const preventEventsMaybe = (info: any, event: any): void => {
   }
   if (info.stopPropagation) {
     event.stopPropagation()
-  }
-  const { target, dataTransfer } = event
-  if (dataTransfer) {
-    const uid = getComponentUid(target)
-    const dragInfo = getDragInfo(uid)
-    if (!dragInfo) {
-      return
-    }
-    for (const item of dragInfo) {
-      dataTransfer.setData(item.type, item.data)
-    }
-    if (dragInfo.label) {
-      setDragImage(dataTransfer, dragInfo.label)
-    }
   }
 }
