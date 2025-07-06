47eca8ae707591bed1a6cf4615af5aa83c35b507
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-06 11:07:31 +0200

    feature: query index of drag data (#236)
    * feature: query index of drag data
    
    * coverage

--- packages/virtual-dom/src/parts/PreventEventsMaybe/PreventEventsMaybe.ts
+++ packages/virtual-dom/src/parts/PreventEventsMaybe/PreventEventsMaybe.ts
@@ -2,6 +2,18 @@ const isInputElement = (element: HTMLElement): boolean => {
   return element instanceof HTMLInputElement
 }
 
+const getDragTargetIndex = (target: HTMLElement): number => {
+  if (target.dataset.index) {
+    return Number.parseInt(target.dataset.index)
+  }
+  const parentNode = target.closest('[data-index]')
+  if (!parentNode) {
+    return -1
+  }
+  // @ts-ignore
+  return Number.parseInt(parentNode.dataset.index)
+}
+
 export const preventEventsMaybe = (info: any, event: any): void => {
   if (info.preventDefault === 2) {
     if (!isInputElement(event.target)) {
@@ -14,9 +26,10 @@ export const preventEventsMaybe = (info: any, event: any): void => {
     event.stopPropagation()
   }
   if (event.dataTransfer) {
+    const index = getDragTargetIndex(event.target)
     if (info.dragInfo) {
       for (const item of info.dragInfo) {
-        event.dataTransfer.setData(item.type, item.data)
+        event.dataTransfer.setData(item.type, item.data + index)
       }
     }
     if (info.dragEffect) {
