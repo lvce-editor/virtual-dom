243747ed0f7e8a67772914d53153d4a1e613afb3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-15 16:43:09 +0200

    feature: use dom node type (#250)

--- packages/virtual-dom/src/parts/VirtualDomElement/VirtualDomElement.ts
+++ packages/virtual-dom/src/parts/VirtualDomElement/VirtualDomElement.ts
@@ -1,3 +1,4 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ElementTagMap from '../ElementTagMap/ElementTagMap.ts'
 import * as VirtualDomElementProps from '../VirtualDomElementProps/VirtualDomElementProps.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
@@ -7,7 +8,7 @@ const renderDomTextNode = (element: any): Text => {
 }
 
 const renderDomElement = (
-  element: any,
+  element: VirtualDomNode,
   eventMap: any,
   newEventMap: any,
 ): HTMLElement => {
@@ -18,7 +19,7 @@ const renderDomElement = (
 }
 
 export const render = (
-  element: any,
+  element: VirtualDomNode,
   eventMap: any,
   newEventMap?: any,
 ): Node => {
