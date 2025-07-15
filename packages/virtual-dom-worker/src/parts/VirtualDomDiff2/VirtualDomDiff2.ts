c4ff98d544c33e7989c6765210e0cd05a3c8f0bc
Le Vivilet <levivlet@pm.me>
2025-07-15 16:46:21 +0200

    add tree

--- packages/virtual-dom-worker/src/parts/VirtualDomDiff2/VirtualDomDiff2.ts
+++ packages/virtual-dom-worker/src/parts/VirtualDomDiff2/VirtualDomDiff2.ts
@@ -6,10 +6,28 @@ import * as GetTotalChildCount from '../GetTotalChildCount/GetTotalChildCount.ts
 import * as PatchType from '../PatchType/PatchType.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
+const createTree = (nodes: readonly VirtualDomNode[]): any => {
+  const max = nodes.length - 1
+  let stack: VirtualDomNode[] = []
+  for (let i = max; i >= 0; i--) {
+    const element = nodes[i]
+    const Element = { ...element, children: stack.slice(0, element.childCount) }
+    stack = stack.slice(element.childCount)
+    stack.unshift(Element)
+  }
+  const root = {
+    type: 0,
+    children: stack,
+  }
+  return root
+}
+
 export const diff = (
   oldNodes: readonly VirtualDomNode[],
   newNodes: readonly VirtualDomNode[],
 ): readonly Patch[] => {
+  const a = createTree(oldNodes)
+  const b = createTree(newNodes)
   const patches: Patch[] = []
   return patches
 }
