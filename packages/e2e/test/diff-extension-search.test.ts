9a7f70ded468bc193ed51c9169117a71e005e53b
Le Vivilet <levivilet@pm.me>
2026-01-23 13:13:56 +0100

    feature: add extension search diff e2e test

---
+++ packages/e2e/test/diff-extension-search.test.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '../src/fixtures.ts'
+
+test('diff - extension search', async ({ page }) => {
+  await page.goto('/diff/extension-search.html')
+
+  await page.waitForFunction(() => {
+    // @ts-ignore
+    return globalThis.__virtualDomDiffTestComplete === true
+  })
+
+  const container = page.locator('#diff-container')
+  const innerHTML = await container.innerHTML()
+  expect(innerHTML).toBe('<div><span>First</span><span>Second</span></div>')
+})
