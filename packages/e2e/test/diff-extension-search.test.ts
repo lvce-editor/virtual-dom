416201caf90a322d0dc604cc75e59610473e501a
Le Vivilet <levivilet@pm.me>
2026-01-23 13:16:38 +0100

    skip

--- packages/e2e/test/diff-extension-search.test.ts
+++ packages/e2e/test/diff-extension-search.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '../src/fixtures.ts'
 
-test('diff - extension search', async ({ page }) => {
+test.skip('diff - extension search', async ({ page }) => {
   await page.goto('/diff/extension-search=results.html')
 
   await page.waitForFunction(() => {
