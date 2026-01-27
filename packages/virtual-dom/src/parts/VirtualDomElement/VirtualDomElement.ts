a29fb6d1341c40684167093669ce200c4b613e9d
Le Vivilet <levivilet@pm.me>
2026-01-27 18:32:37 +0100

    feature: support reference nodes in rendering (#370)
    * feature: support reference nodes in rendering
    
    * fix

--- packages/virtual-dom/src/parts/VirtualDomElement/VirtualDomElement.ts
+++ packages/virtual-dom/src/parts/VirtualDomElement/VirtualDomElement.ts
@@ -1,5 +1,6 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ElementTagMap from '../ElementTagMap/ElementTagMap.ts'
+import * as Instances from '../Instances/Instances.ts'
 import * as VirtualDomElementProps from '../VirtualDomElementProps/VirtualDomElementProps.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
@@ -18,12 +19,20 @@ const renderDomElement = (
   return $Element
 }
 
+const renderReferenceNode = (element: VirtualDomNode): HTMLElement => {
+  const instance = Instances.get(element.uid)
+  const $Node = instance.state.$Viewlet
+  return $Node
+}
+
 export const render = (
   element: VirtualDomNode,
   eventMap: any,
   newEventMap?: any,
 ): Node => {
   switch (element.type) {
+    case VirtualDomElements.Reference:
+      return renderReferenceNode(element)
     case VirtualDomElements.Text:
       return renderDomTextNode(element)
     default:
