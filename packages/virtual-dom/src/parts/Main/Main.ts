cdea1654882b31aa0cbebf705ea78f8318cd4b5a
Le Vivilet <levivilet@pm.me>
2026-02-21 14:31:28 +0100

    feature: add IDragInfo import and export in Main.ts (#406)
    * feature: add IDragInfo import and export in Main.ts
    
    * tsc

--- packages/virtual-dom/src/parts/Main/Main.ts
+++ packages/virtual-dom/src/parts/Main/Main.ts
@@ -10,10 +10,13 @@ export {
   setDragInfo,
 } from '../DragInfo/DragInfo.ts'
 export { addFileHandle, getFileHandles } from '../FileHandles/FileHandles.ts'
+export type * from '../IDragInfo/IDragInfo.ts'
+export {
+  get as getViewletInstance,
+  set as setViewletInstance,
+} from '../Instances/Instances.ts'
 export { setIpc } from '../IpcState/IpcState.ts'
 export { registerEventListeners } from '../RegisterEventListeners/RegisterEventListeners.ts'
 export { rememberFocus } from '../RememberFocus/RememberFocus.ts'
 export { render, renderInto } from '../VirtualDom/VirtualDom.ts'
 export * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-export { set as setViewletInstance } from '../Instances/Instances.ts'
-export { get as getViewletInstance } from '../Instances/Instances.ts'
