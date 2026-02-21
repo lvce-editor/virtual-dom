a86ab1d478d241d6a35ed3b146cfa57da2f4d67f
Le Vivilet <levivilet@pm.me>
2026-02-21 22:23:43 +0100

    test: enable various changes test and update expected innerHTML

--- packages/e2e/test/various-changes.test.ts
+++ packages/e2e/test/various-changes.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '../src/fixtures.ts'
 
-test.skip('diff - various changes', async ({ page }) => {
+test('diff - various changes', async ({ page }) => {
   await page.goto('/diff/various-changes.html')
 
   await page.waitForFunction(() => {
@@ -10,5 +10,7 @@ test.skip('diff - various changes', async ({ page }) => {
 
   const container = page.locator('#diff-container')
   const innerHTML = await container.innerHTML()
-  expect(innerHTML).toBe('<div class="new-class" id="new-id">Hello</div>')
+  expect(innerHTML).toBe(
+    '<div class="Main"><div class="EditorGroup" style="width: 100%;"><div class="EditorGroupHeader" role="none"><div class="MainTabs" role="tablist"><div class="MainTab MainTabSelected" data-group-index="0" data-index="0" role="tab" title="memfs:///workspace/file1.ts"><img class="TabIcon" role="none" src="extensions/builtin.vscode-icons/icons/file_type_typescript.svg"><span class="TabTitle">file1.ts</span><button class="EditorTabCloseButton" data-group-index="0" data-index="0"><div class="MaskIcon MaskIconClose"></div></button></div></div></div><div class="EditorContainer"><button class="Editor"></button></div></div></div>',
+  )
 })
