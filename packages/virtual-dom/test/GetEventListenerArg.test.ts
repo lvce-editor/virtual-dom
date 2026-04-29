6552c720836559408cd046a1096c283fe8b90c90
Le Vivilet <levivilet@pm.me>
2026-04-29 23:42:45 +0200

    feature: add handleClipboardDataFiles function and corresponding tests for clipboard file handling (#445)
    * feature: add handleClipboardDataFiles function and corresponding tests for clipboard file handling
    
    * fix
    
    * fix

---
+++ packages/virtual-dom/test/GetEventListenerArg.test.ts
@@ -0,0 +1,42 @@
+/**
+ * @jest-environment jsdom
+ */
+import { expect, test } from '@jest/globals'
+import { getEventListenerArg } from '../src/parts/GetEventListenerArg/GetEventListenerArg.ts'
+
+test('getEventListenerArg - event.clipboardData.files returns pasted files array', () => {
+  const firstFile = new File(['a'], 'first.txt', {
+    type: 'text/plain',
+  })
+  const secondFile = new File(['b'], 'second.txt', {
+    type: 'text/plain',
+  })
+  const event = {
+    clipboardData: {
+      items: [
+        {
+          kind: 'string',
+          getAsFile: (): null => null,
+        },
+        {
+          kind: 'file',
+          getAsFile: (): File => firstFile,
+        },
+        {
+          kind: 'file',
+          getAsFile: (): File => secondFile,
+        },
+      ],
+    },
+  }
+
+  const result = getEventListenerArg('event.clipboardData.files', event)
+
+  expect(result).toEqual([firstFile, secondFile])
+})
+
+test('getEventListenerArg - event.clipboardData.files returns empty array without clipboard data', () => {
+  const result = getEventListenerArg('event.clipboardData.files', {})
+
+  expect(result).toEqual([])
+})
