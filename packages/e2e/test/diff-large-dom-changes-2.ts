1d166074894c32724604ec6593534bb695f587d4
Le Vivilet <levivilet@pm.me>
2026-06-23 10:45:05 +0200

    feature: add another large dom test

---
+++ packages/e2e/test/diff-large-dom-changes-2.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '../src/fixtures.ts'
+
+test.skip('diff - multiple attribute changes', async ({ page }) => {
+  await page.goto('/diff/large-dom-changes-2.html')
+
+  await page.waitForFunction(() => {
+    // @ts-ignore
+    return globalThis.__virtualDomDiffTestComplete === true
+  })
+
+  const container = page.locator('#diff-container')
+  const innerHTML = await container.innerHTML()
+  expect(innerHTML).toBe(
+    '<div class="new-class" id="new-id" data-test="value">Content</div>',
+  )
+})
