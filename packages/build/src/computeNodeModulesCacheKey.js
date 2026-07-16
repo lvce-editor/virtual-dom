c07484be0241d780ad54c459208ab203d055b02c
Le Vivilet <levivilet@pm.me>
2026-07-16 12:35:23 +0000

    feature: migrate to npm workspaces

--- packages/build/src/computeNodeModulesCacheKey.js
+++ packages/build/src/computeNodeModulesCacheKey.js
@@ -1,35 +1,16 @@
 import { createHash } from 'node:crypto'
-import { readdirSync } from 'node:fs'
 import { readFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { root } from './root.js'
 
-const getPackageLocations = () => {
-  const packageLocations = []
-  const packagesFolder = join(root, 'packages')
-  const dirents = readdirSync(packagesFolder)
-  for (const dirent of dirents) {
-    packageLocations.push(`packages/${dirent}/package-lock.json`)
-  }
-  packageLocations.push('package-lock.json')
-  return packageLocations
-}
-
 const locations = [
-  'lerna.json',
-  ...getPackageLocations(),
+  'package.json',
+  'package-lock.json',
   '.github/workflows/pr.yml',
   '.github/workflows/ci.yml',
   '.github/workflows/release.yml',
   'packages/build/src/computeNodeModulesCacheKey.js',
 ]
-
-const packagesFolder = join(root, 'packages')
-
-const dirents = readdirSync(packagesFolder)
-for (const dirent of dirents) {
-  locations.push(`packages/${dirent}/package-lock.json`)
-}
 
 const getAbsolutePath = (relativePath) => {
   return join(root, relativePath)
