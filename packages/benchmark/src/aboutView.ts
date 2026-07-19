488708602ee13226ec92cf457919e044d52e71cd
Le Vivilet <levivilet@pm.me>
2026-07-19 17:59:22 +0000

    fix: update about view benchmark

--- packages/benchmark/src/aboutView.ts
+++ packages/benchmark/src/aboutView.ts
@@ -7,8 +7,8 @@ const temporaryRoot = join(packageRoot, '.tmp')
 
 export const getAboutViewTests = async (): Promise<BenchmarkTests> => {
   return getBenchmarkTests({
-    defaultCommit: 'b69adc3b3f9bc0dbb7fd0733234ec51be87e3f6b',
-    defaultRef: 'v7.9.0',
+    defaultCommit: 'e793388bfec0c088f9cf48e9d99d207b91948cfc',
+    defaultRef: 'v7.10.0',
     downloadRoot: join(temporaryRoot, 'about-view'),
     id: 'about-view',
     label: 'About',
